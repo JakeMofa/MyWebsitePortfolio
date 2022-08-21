dbe8a5b1258ac69451a150d2dc1b91bc5a2b51dd
Jake Mofa <jakemofa@gmail.com>
2022-08-20 18:28:58 -0700

    Add files via upload

---
+++ GWeb/jakemofaportfolio/staticfiles/JS/app.js
@@ -0,0 +1,18 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo("", { opacity: 0 }, { opacity: 1, duration: 1 });
+
+// const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+
+// tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+// tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5});
+// tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+// tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+// tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+// tl.fromTo(".profile-wrapper", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "=-2");
+// tl.fromTo(".container", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "=-2");
